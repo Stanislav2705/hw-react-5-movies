94d900b980be57ed629276fe0feb41b35ee650d2
Stanislav2705 <stasrogal25@gmail.com>
2022-11-21 16:28:13 +0200

    movie-details

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Home from "pages/Home";
+import MovieDetails from "pages/MovieDetails";
 import Movies from "pages/Movies";
 import { Route, Routes } from "react-router-dom";
 import PageContextProvider from "shared/PageContext/PageContext";
@@ -12,7 +13,7 @@ export const App = () => {
         <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>} />
         <Route path="/movies" element={<Movies/>} />
-          <Route path="/movies/:movieId" element={<div>MovieDetails</div>}>
+          <Route path="/movies/:movieId" element={<MovieDetails/>}>
             <Route path="cast" element={<div>Cast</div>} />
             <Route path="reviews" element={<div>Reviews</div>} />
         </Route>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,9 @@
 import { getMovieDetails } from "fakeApi";
-import { useState, useEffect } from "react"
-import { useLocation, useParams } from "react-router-dom";
+import { useState, useEffect, Suspense } from "react"
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import defaultImage from '../Image/default-poster.jpg'
+import { IoIosArrowBack } from "react-icons/io";
+import Loader from "shared/Loader";
 
 
 export default function MovieDetails() {
@@ -10,6 +13,7 @@ export default function MovieDetails() {
   const { movieId } = useParams();
   const location = useLocation();
 
+  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
   useEffect(() => {
     async function movieById() {
@@ -31,8 +35,52 @@ export default function MovieDetails() {
   const isReviews = location.pathname.includes('reviews');
   const reviewsLink = isReviews ? `/movies/${movieId}` : 'reviews';
 
+  const { original_title, genres, overview, poster_path, release_date, vote_average } = movie;
+  const genre = genres?.map(a => a.name).join(', ');
+  const voteAverage = Math.ceil(vote_average * 10);
+  const date = release_date?.slice(0, 4);
+  const backLink = location.state?.from ?? '/movies';
+  const IMG = poster_path ? `${IMG_URL}/${poster_path}` : defaultImage;
 
   return (
-    <div>MovieDetails</div>
+    <>
+      <Link to={backLink}>
+        <IoIosArrowBack/>
+        Go back
+      </Link>
+
+      {loading && <Loader />}
+      <div>
+        <img src={IMG} width="300" alt={original_title} />
+        <div>
+          <title>
+            {original_title} ({date ? date : "NOT TITLE"})
+          </title>
+          <p>User score: {voteAverage}%</p>
+          <p>Overview</p>
+          <p>{overview}</p>
+          <p>Genres</p>
+          <p>{genre}</p>
+        </div>
+        </div>
+        <div>
+          <title>Additional information</title>
+          <ul>
+            <li>
+              <Link to={castLink} state={{ from: backLink }}>
+                Cast
+              </Link>
+            </li>
+            <li>
+              <Link to={reviewsLink} state={{ from: backLink }}>
+                Reviews
+              </Link>
+            </li>
+          </ul>
+        </div>
+      <Suspense fallback={null}>
+        <Outlet/>
+      </Suspense>
+    </>
   )
 }
